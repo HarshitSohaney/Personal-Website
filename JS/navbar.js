98700b1290cbe5e28eade42c6787d4626e2c98ab
Harshit Sohaney <harshit.sohaney@mail.utoronto.ca>
2022-08-25 18:19:28 -0500

    Made navbar scrollable to

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,17 +1,15 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-} 
+$(document).ready(function() {
+  
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).scrollTop())
+      if ($(window).scrollTop() > 280) {
+        $('#navbar').addClass('navbar-fixed');
+      }
+      if ($(window).scrollTop() < 281) {
+        $('#navbar').removeClass('navbar-fixed');
+      }
+    });
+  });
