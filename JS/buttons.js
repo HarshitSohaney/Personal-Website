10889aec44ebd4d2474da2f6fa637dd850fa9452
Harshit Sohaney <harshit.sohaney@mail.utoronto.ca>
2022-08-30 11:51:01 -0500

    Working on vertical align

--- JS/buttons.js
+++ JS/buttons.js
@@ -3,19 +3,3 @@ $(document).ready(function(){
         $(this).attr("src", "icons/shuffle-green.png");
     });
 });
-
-//Accordian
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
