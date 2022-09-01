9141a6187ae6cc06a7d7df7f357f2429bb3b36b6
Harshit Sohaney <harshit.sohaney@mail.utoronto.ca>
2022-09-01 10:28:10 -0500

    changed navbar catch

--- JS/navbar.js
+++ JS/navbar.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
         //if you hard code, then use console
         //.log to determine when you want the 
         //nav bar to stick.  
-        //console.log($(window).scrollTop())
-      if ($(window).scrollTop() > 381) {
+        console.log($(window).scrollTop())
+      if ($(window).scrollTop() > 341) {
         $('#navbar').addClass('navbar-fixed');
       }
-      if ($(window).scrollTop() < 380) {
+      if ($(window).scrollTop() < 340) {
         $('#navbar').removeClass('navbar-fixed');
       }
     });
